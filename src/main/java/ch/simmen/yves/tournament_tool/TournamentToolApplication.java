aa147db4ae2327c45fd8b59f859442130ebde56d
Yves Simmen <yves.simmen@students.fhnw.ch>
2018-10-10 14:42:22 +0200

    Added generated spring boot project

---
+++ src/main/java/ch/simmen/yves/tournament_tool/TournamentToolApplication.java
@@ -0,0 +1,12 @@
+package ch.simmen.yves.tournament_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentToolApplication.class, args);
+	}
+}
---
+++ src/test/java/ch/simmen/yves/tournament_tool/TournamentToolApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.simmen.yves.tournament_tool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TournamentToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
